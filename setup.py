8f6526538ea5d99f5bd92c89ff6495f072d7f59c
marvol0 <murithimarvin09@gmail.com>
2025-03-02 22:13:49 +0300

    feat(build): add package metadata from build configurations

--- setup.py
+++ setup.py
@@ -4,3 +4,11 @@ from setuptools import setup, find_namespace_packages
 
 VERSION = os.environ.get("SNAPATTACK_VERSION", "0.1")
 
+PACKAGE_INFO = {
+    "name": "snapattack",
+    "version": 0.1,
+    "description": "A lightweight Python-based event collection and publishing system, designed for streamlined data processing. It includes a modular architecture for gathering, processing, and forwarding events, with optional Snap packaging for easy deployment.",
+    "author": "Marvin Murithi",
+    "license": "MIT",
+    "url": "https://snapcraft.io/snapattack",
+}
